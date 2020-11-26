5722102661a67c01b33e41f3792744f1aec2f698
winning0804 <574570184@qq.com>
2020-11-26 23:34:58 +0800

    物品界面详情的修改

--- 小程序/pages/return/return.js
+++ 小程序/pages/return/return.js
@@ -6,24 +6,24 @@ Page({
    */
   data: {
       id:"",
-      page:"已借出",
+      page:"我是xx人",
       array:[{
+        num:"1",
         img:"../../images/u8.png",
         obj:"电子产品|蓝牙耳机",
         people:"借用人：美少女战士",
         price:"￥3.00/日",
         date:"20.11.11-20.11.13",
-        button1:"提醒归还",
-        button2:"确认归还",
+        situa:"待审核"
       },
       {
+        num:"2",
         img:"../../images/电动车.png",
         obj:"交通出行|电动车",
         people:"借用人：圣代",
         price:"￥5.00/日",
         date:"20.10.10-20.11.11",
-        button1:"提醒归还",
-        button2:"确认归还"
+        button1:"待交易"
       }
     ]
     
@@ -36,117 +36,61 @@ Page({
     var that = this;
     if(options.id==1){
       that.setData({
-        page:"已借出",
+        page:"我是所属人",
         id:"1",
         array:[{
+          num:"1",
           img:"../../images/u8.png",
           obj:"电子产品|蓝牙耳机",
           people:"借用人：美少女战士",
           price:"￥3.00/日",
           date:"20.11.11-20.11.13",
-          button1:"提醒归还",
-          button2:"确认归还",
+          button1:"待审核"
         },
         {
+          num:"3",
           img:"../../images/电动车.png",
           obj:"交通出行|电动车",
           people:"借用人：圣代",
           price:"￥5.00/日",
           date:"20.10.10-20.11.11",
-          button1:"提醒归还",
-          button2:"确认归还"
+          situa:"等待对方确认交易"
         }
       ]
       })
     }
     if(options.id==2){
       that.setData({
-        page:"待归还",
+        page:"我是借方",
         id:"2",
         array:[{
+          num:"2",
           img:"../../images/电动车.png",
           obj:"交通出行|电动车",
           people:"所属人：月野兔",
           price:"￥10.00/日",
           date:"20.08.04-20.11.23",
-          button2:"归还",
+          button1:"待交易",
         },
         {
+          num:"5",
           img:"../../images/指甲剪.png",
           obj:"日常用品|指甲剪",
           people:"所属人：subs",
           price:"￥1.00/日",
           date:"20.10.04-20.11.23",
-          button2:"归还",
+          situa:"等待对方确认归还"
         },
         {
+          num:"1",
           img:"../../images/电动车.png",
           obj:"交通出行|电动车",
           people:"所属人：月野兔",
           price:"￥10.00/日",
           date:"20.08.04-20.11.23",
-          button2:"归还"
+          situa:"待审核"
         }
         ]
-      })
-    }
-    if(options.id==3){
-      that.setData({
-        page:"待处理",
-        id:"3",
-        array:[{
-          img:"../../images/快递.png",
-          obj:"服务|代取快递",
-          people:"借用人：懒惰",
-          price:"￥2.00/日",
-          date:"20.10.04-20.11.27",
-          button2:"确认借出"
-        }
-        ]
-      })
-    }
-    if(options.id==4){
-      that.setData({
-        page:"待审核",
-        id:"4",
-        array:[{
-          img:"../../images/指甲剪.png",
-          obj:"日常用品|指甲剪",
-          people:"所属人：家长",
-          price:"￥1.00/日",
-          date:"20.11.04-20.11.22",
-          button1:"取消申请",
-          button2:"确认借到",
-        },
-        {
-          img:"../../images/指甲剪.png",
-          obj:"日常用品|指甲剪",
-          people:"所属人：家长",
-          price:"￥1.00/日",
-          date:"20.11.04-20.11.22",
-          button1:"取消申请",
-          button2:"确认借到",
-        },
-        {
-          img:"../../images/指甲剪.png",
-          obj:"日常用品|指甲剪",
-          people:"所属人：家长",
-          price:"￥1.00/日",
-          date:"20.11.04-20.11.22",
-          button1:"取消申请",
-          button2:"确认借到",
-        },
-        {
-          img:"../../images/指甲剪.png",
-          obj:"日常用品|指甲剪",
-          people:"所属人：家长",
-          price:"￥1.00/日",
-          date:"20.11.04-20.11.22",
-          button1:"取消申请",
-          button2:"确认借到"
-        }
-        ]
-        
       })
     }
     if(options.id==5){
@@ -215,8 +159,8 @@ Page({
 
   },
 
-  obj_detail:function(){
-    console.log(this.data.id);
+  obj_detail:function(e){
+    console.log(e);
     
     if(this.data.id==1){
       wx.navigateTo({
