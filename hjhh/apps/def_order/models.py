ca77a30a3ac9e8fb7d67e142375685c4409625d2
winning0804 <574570184@qq.com>
2020-11-26 22:54:53 +0800

    后端订单部分更新

--- hjhh/apps/def_order/models.py
+++ hjhh/apps/def_order/models.py
@@ -10,19 +10,19 @@ class Order(models.Model):
         (4,'待归还'),
         (5,'等待对方确认归还')
     )
-    belonger = models.ForeignKey(verbose_name='所属人',to='UserInfo')
-    user = models.ForeignKey(verbose_name='借方', to='UserInfo')
-    goods = models.ForeignKey(verbose_name='商品',to='GoodsSKU')
+    belonger = models.ForeignKey(verbose_name='所属人',to='UserInfo',on_delete=models.CASCADE)
+    user = models.ForeignKey(verbose_name='借方', to='UserInfo',on_delete=models.CASCADE)
+    goods = models.ForeignKey(verbose_name='商品',to='GoodsSKU',on_delete=models.CASCADE)
     status = models.SmallIntegerField(verbose_name='状态',choices=status_choices)
     startdate = models.DateTimeField(verbose_name='出借时间')
     enddate = models.DateTimeField(verbose_name='归还时间')
-    money = models.CharField(verbose_name='交易金额')
+    money = models.CharField(verbose_name='交易金额',max_length=10)
 
 
 class Comments(models.Model):
     #评论
-    user = models.ForeignKey(verbose_name='评论者', to='UserInfo')
-    goods = models.ForeignKey(verbose_name='商品', to='GoodsSKU')
-    stars = models.CharField(verbose_name='评星')
+    user = models.ForeignKey(verbose_name='评论者', to='UserInfo',on_delete=models.CASCADE)
+    goods = models.ForeignKey(verbose_name='商品', to='GoodsSKU',on_delete=models.CASCADE)
+    stars = models.CharField(verbose_name='评星',max_length=1)
     comment = models.TextField(verbose_name='评论',max_length=100)
     comment_time = models.DateTimeField(verbose_name='评论时间')
