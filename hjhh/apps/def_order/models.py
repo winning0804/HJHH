b6ab6bf2ceca53e1a0dc6cb4e4e544bd2e8a7490
pollia <3152668032@qq.com>
2020-11-27 16:11:14 +0800

    初步整合完成
    不报错了！！！

--- hjhh/apps/def_order/models.py
+++ hjhh/apps/def_order/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from def_goods.models import GoodsSKU
+from def_user.models import UserInfo
+import sys
+sys.path.append("..")
 # Create your models here.
 class Order(models.Model):
     #订单表
@@ -10,9 +13,9 @@ class Order(models.Model):
         (4,'待归还'),
         (5,'等待对方确认归还')
     )
-    belonger = models.ForeignKey(verbose_name='所属人',to='UserInfo',on_delete=models.CASCADE)
-    user = models.ForeignKey(verbose_name='借方', to='UserInfo',on_delete=models.CASCADE)
-    goods = models.ForeignKey(verbose_name='商品',to='GoodsSKU',on_delete=models.CASCADE)
+    belonger = models.ForeignKey(UserInfo,verbose_name='所属人',on_delete=models.CASCADE,related_name='user')
+    user = models.ForeignKey(UserInfo,verbose_name='借方',on_delete=models.CASCADE,related_name='belonger')
+    goods = models.ForeignKey(GoodsSKU,verbose_name='商品',on_delete=models.CASCADE)
     status = models.SmallIntegerField(verbose_name='状态',choices=status_choices)
     startdate = models.DateTimeField(verbose_name='出借时间')
     enddate = models.DateTimeField(verbose_name='归还时间')
@@ -21,8 +24,8 @@ class Order(models.Model):
 
 class Comments(models.Model):
     #评论
-    user = models.ForeignKey(verbose_name='评论者', to='UserInfo',on_delete=models.CASCADE)
-    goods = models.ForeignKey(verbose_name='商品', to='GoodsSKU',on_delete=models.CASCADE)
+    user = models.ForeignKey(UserInfo,verbose_name='评论者',on_delete=models.CASCADE)
+    goods = models.ForeignKey(GoodsSKU,verbose_name='商品',on_delete=models.CASCADE)
     stars = models.CharField(verbose_name='评星',max_length=1)
     comment = models.TextField(verbose_name='评论',max_length=100)
     comment_time = models.DateTimeField(verbose_name='评论时间')
