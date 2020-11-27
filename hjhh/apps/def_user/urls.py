2520d831555b72ba4c325562494054546c34c11f
huangjiaying0731 <70759935+huangjiaying0731@users.noreply.github.com>
2020-11-27 23:04:10 +0800

    Update urls.py

--- hjhh/apps/def_user/urls.py
+++ hjhh/apps/def_user/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 app_name = 'def_user'
 urlpatterns = [
-    url(r'^user_list',views.user_list.as_view()),
     url(r'^user_detail', views.user_detail.as_view()),
     url(r'^getUserInfo', views.getUserInfo.as_view())
 ]
