3dfd7e3e758f88ae9e20dc17ab350d4505efd5ed
yoosung <49530758+Artemis05@users.noreply.github.com>
2020-11-27 22:55:11 +0800

    Update urls.py

--- hjhh/apps/def_message/urls.py
+++ hjhh/apps/def_message/urls.py
@@ -1,13 +1,8 @@
-import os,django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hjhh.settings")# project_name 项目名称
-django.setup()
 
 from django.conf.urls import url
 from . import views
 
 app_name = 'def_message'
 urlpatterns = [
-        url('api/get_message',views.message, name="message"),
-        url('api/get_message_detail', views.message_detail, name="message_detail"),
-        # url(r'^kefu_message/$', kefu_message, name="kefu_message"),
+      
 ]
