6e5491f89d1c6b68bd907165ce7485cc1325acbf
huangjiaying0731 <70759935+huangjiaying0731@users.noreply.github.com>
2020-11-30 08:38:09 +0800

    Update views.py

--- hjhh/apps/def_user/views.py
+++ hjhh/apps/def_user/views.py
@@ -4,8 +4,44 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 import json
+from .wx_login import *
+from django.core import cache
+import hashlib,time
+from .models import UserInfo
+
 # Create your views here
 
+#授权用户登录
+class Login(APIView):
+    def post(self,request):
+        param = request.data
+        if not param.get('code'):
+            return Response({'status':1,"msg":"缺少参数"})
+        else:
+            code = param.get('code')
+            user_data = login(code)
+            if user_data:
+                val = user_data['session_key'] + "&" + user_data['openid']
+                md5 = hashlib.md5()
+                md5.update(str(time.clock()).encode('utf-8'))
+                md5.update(user_data['session_key'].encode('utf-8'))
+                key = md5.hexdigest()
+                cache.set(key,val)
+                has_user = UserInfo.objects.filter(openID=user_data['openid']).first()
+                if not has_user:
+                    UserInfo.objects.create(openID=user_data['openid'])
+                UserInfo.objects.update()
+                return Response(
+                    {
+                        'status':0,
+                        'msg':'ok',
+                        'data':{'token':key}
+                    }
+                )
+            else:
+                return Response({'status':2,'msg':"无效的code"})
+
+            
 # 针对特定用户进行信息修改操作
 class user_detail(APIView):
     def get_objects(self,pk):
