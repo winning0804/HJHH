aa7f2ed20e282d587c8169cbf568c13c7a2f2d70
winning0804 <574570184@qq.com>
2020-12-23 00:46:58 +0800

    ui

--- 小程序/pages/message/message.js
+++ 小程序/pages/message/message.js
@@ -7,34 +7,12 @@ Page({
    
     messages:[
       { 
-        nameid:'1',
-        borrower_name:"美少女战士",
-        url:"../../images/u22.svg",
-        borrower_message:"租金可以便宜点吗？",
-        count:5
-      },
-      {
-        nameid:'2',
-        borrower_name:"房东的猫",
-        url:"../../images/u22.svg",
-        borrower_message:"哈哈哈，这个耳机质量怎么样？",
-        count:8
-        },
-        {
-          nameid:'3',
-          borrower_name:"马化腾",
-          url:"../../images/gerenzhongxin.png",
-          borrower_message:"在吗?想借电动车。",
-          count:2
-    
-          },
-          {
-            nameid:'4',
-            borrower_name:"马云",
-            url:"../../images/点赞2.png",
-            borrower_message:"我现在没时间，等下聊",
-            count:4
-            },
+        img:"https://thirdwx.qlogo.cn/mmopen/vi_32/IicYPOQN5rpniakqB4pHM56P3ajicTNHJr62yOmMVSKsic950JKkATIDicicGyhruGuxaAEVcJ2HrIzdCQTsr7pZhK0g/132",
+        name:"耳机军",
+        text:"你好牙",
+        time:"22：20",
+        tag:"5"
+      }
      
     ]
   },
--- 小程序/pages/message_detail/message_detail.js
+++ 小程序/pages/message_detail/message_detail.js
@@ -5,26 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    url:" ",
-    messages_detail_text:" ",
-    name:" ",
-    ur1:"../../images/u22.svg",
-    ur2:"../../images/mine-image.png",
-    messages_detail_text1:[
-      "租金可以便宜点吗？"
-    ],
-    messages_detail_text2:[
-      "可以啊 ","多少你能接受？"
-    ]
+    InputBottom: 0
+  },
+  InputFocus(e) {
+    this.setData({
+      InputBottom: e.detail.height
+    })
+  },
+  InputBlur(e) {
+    this.setData({
+      InputBottom: 0
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      url:options.image,
-      name:options.name
-    })
+    console.log(options);
     
    
   },
